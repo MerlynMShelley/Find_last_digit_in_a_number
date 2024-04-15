a4f966eacdf7042827041a9baa20697aa82eae1c
Merlyn Shelley <merlynmshelley@gmail.com>
2024-04-15 11:17:34 +0530

    Weight Calc program

--- Weight Calc.py
+++ Weight Calc.py
@@ -2,7 +2,7 @@ weight = float(input("Enter your body weight:"))
 unit = input("Is it (L)bs or (K)gs:")
 if (unit.upper() == "K"):
     new_weight = weight * 2.2
-    print(f"You are {new_weight} kilos")
+    print(f"You are {new_weight} pounds")
 else:
     new_weight = weight/2.2
-    print(f"You are {new_weight} pounds")
+    print(f"You are {new_weight} kilos")
