6e3df6614a429306e49e717cec416bcbb74b807e
Merlyn Shelley <merlynmshelley@gmail.com>
2024-04-15 11:34:23 +0530

    Guess a number

---
+++ Guess a number.py
@@ -0,0 +1,13 @@
+secret_number = 7
+guess_count = 0
+guess_limit = 3
+while (guess_count < guess_limit):
+    guess_number = int(input("Make you Guessing number:"))
+    guess_count += 1
+    if (guess_number == 9):
+        print("You Won!")
+        break
+else:
+    print("Sorry, you lost!")
+
+
